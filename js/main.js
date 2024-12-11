3400aa7059ba834deeedd18c89fb60b31a50acfe
mahmouddessoki <mahmouddessoki33@gmail.com>
2024-12-11 19:20:08 +0200

    swiper added using swiperjs library

--- js/main.js
+++ js/main.js
@@ -12,13 +12,54 @@ function hideToggle() {
     toggleIcon.style.display = "none"
 }
 
-function showToggle() { 
+function showToggle() {
     toggleIcon.style.display = "block"
 
 }
 function showDeepMenu() {
     console.log("done");
-    
+
 }
 
 
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    slidesPerView: 6,
+    spaceBetween: 30,
+    freeMode: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 5,
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 25,
+        },
+        1400: {
+            slidesPerView: 6,
+            spaceBetween: 30,
+        },
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
